53eeac1d9be502e97435d403e0fa1d46aef7c391
pirtor <pirtorle@gmail.com>
2024-02-17 11:22:53 +0800

    build: change to static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 import { withContentlayer } from "next-contentlayer";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+};
 
 export default withContentlayer(nextConfig);
